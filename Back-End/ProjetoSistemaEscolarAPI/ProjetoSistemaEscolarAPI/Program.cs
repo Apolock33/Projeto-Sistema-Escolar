7f0130920a06e5937f271929507c76384f92568a
Apolock33 <carlosammgomes@gmail.com>
2023-02-14 13:09:33 -0300

    Projeto de API Reiniciado

--- Back-End/ProjetoSistemaEscolarAPI/ProjetoSistemaEscolarAPI/Program.cs
+++ Back-End/ProjetoSistemaEscolarAPI/ProjetoSistemaEscolarAPI/Program.cs
@@ -1,17 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using ProjetoSistemaEscolarAPI.Models.Context;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddCors((options) => options.AddDefaultPolicy(policy => {
-    policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
-}));
+// Add services to the container.
 
 builder.Services.AddControllers();
-
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,8 +15,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseCors();
 
 app.UseHttpsRedirection();
 
